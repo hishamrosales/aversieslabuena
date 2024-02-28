f38185709f2c63dfb6148039e28dbb42f4b96de6
hishamrosales <hisham.rosales@gmail.com>
2024-02-28 16:36:12 +0100

    claseidididididid

---
+++ ola/ola/Data/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ola.Data.Entities
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Registration { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Student()
+        { 
+        
+        }
+        public int Age()
+        {
+            return 0;
+        }
+    }
+}
